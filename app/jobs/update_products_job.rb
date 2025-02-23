839b7a351568aacc72f6effbb042f4e0eab11324
Shandesh <shandesh@experience.com>
2025-02-23 23:13:39 +0530

    changes in Job file

--- app/jobs/update_products_job.rb
+++ app/jobs/update_products_job.rb
@@ -2,10 +2,24 @@ class UpdateProductsJob < ApplicationJob
   queue_as :default
 
   def perform
+    Rails.logger.info "running job"
     outdated_products = Product.where("last_scraped_at < ?", 1.week.ago)
     outdated_products.each do |product|
       product_data = ScraperService.fetch_product_details(product.url)
-      product.update(product_data.merge(last_scraped_at: Time.current))
+
+      product.update_columns(
+        title: product_data[:title],
+        description: product_data[:description],
+        price: product_data[:price],
+        size: product_data[:size],
+        available_offers: product_data[:available_offers],
+        offer_percentage: product_data[:image_url],
+        rating: product_data[:rating],
+        specifications: product_data[:specifications],
+        no_of_ratings: product_data[:no_of_ratings],
+        no_of_reviews: product_data[:no_of_reviews],
+        last_scraped_at: Time.current
+      )
     end
   end
 end
